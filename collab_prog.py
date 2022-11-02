d43408069c28b53cc90450ab4b7a256e58c69214
KenRoyyy <kroyste1@terpmail.umd.edu>
2022-11-01 21:48:52 -0400

    Update collab_prog.py

--- collab_prog.py
+++ collab_prog.py
@@ -1,11 +1,14 @@
 #This is a practice file for Git and GitHub
 
 def winner(score):
-    if score > 5:
+    if score < 5:
         print("You are the winner")
     else:
         print("You lost")
 
-firstround = {'Player': 'sarah', 'score': 6}
+firstround = {'Player': 'Sarah', 'score': 6}
+secondround = {'Player2': 'Nina', 'score2': 2}
 print(f"The winner of the firstround is {firstround['Player']} with \
-      {firstround['score']} points.")
+{firstround['score']} points.")
+print(f"Unfortunately for the second round, {secondround['Player2']} lost with \
+a score of {secondround['score2']} points.")
