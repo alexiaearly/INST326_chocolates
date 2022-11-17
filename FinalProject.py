885b8c09fc55dc9abaad8b5e04a10a7963c76ed1
Alexia Early <aearly@terpmail.umd.edu>
2022-11-17 18:37:36 -0500

    added the base of the code

--- FinalProject.py
+++ FinalProject.py
@@ -1,1 +1,34 @@
-#This is the beginning
+""" A student class taking enhancer.
+"""
+from argparse import ArgumentParser
+    
+class Notes():
+    def method1():
+        pass
+    def method2():
+        pass
+    def method3():
+        pass
+
+class Planner():
+    def method1():
+        pass
+    def method2():
+        pass
+    def method3():
+        pass
+ 
+def parse_args(arglist):   
+    parser = ArgumentParser()
+    parser.add_argument()
+    parser.add_argument()
+    parser.add_argument()
+    args = parser.parse_args(arglist)
+    return args
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    main(args.file, args.name1, args.name2)
